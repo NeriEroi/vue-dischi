0744963f36c940d1be13a96be4f750e89422d8a0
Michele <micheleierniero@gmail.com>
2021-09-22 16:42:09 +0200

    main completo da migliorare

---
+++ src/components/Disk.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="container">
+
+    <div class="disk_cont">
+               
+        <div class="img_cont">
+        <img class="image" :src="info.poster" alt="info.author">
+        </div>
+        <h5>{{ info.title }}</h5>
+        <div>{{ info.author }}</div>
+        <div>{{ info.year }}</div>
+
+    </div>
+
+</div>
+    
+</template>
+
+<script>
+
+export default {
+    name: 'Disk',
+    props: ['info']
+}
+</script>
+
+<style>
+.disk_cont {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+} .img_cont {
+    width: 8%;
+    height: 8%;
+} .image {
+    width: 100%;
+    height: 100%;
+}
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,46 @@
 <template>
-    <div>
-        
+    <div class="main">
+
+        <Disk v-for="(disk, index) in disksList" :key="index" :info="disk"/>
+
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Disk from './Disk.vue'
 export default {
     name: 'Main',
+    components: {
+        Disk
+    },
+    data() {
+        return {
+           APIUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
+           disksList: []
+        }
+    },
+    created() {
+        this.getDisks();
+    },
+    methods: {
+        getDisks() {
+            axios
+                .get(this.APIUrl)
+                .then( res => {
+                    // console.log(res.data.response);
+                    this.disksList = res.data.response;
+                })
+        }
+    }
 }
 </script>
 
 <style>
+.main {
+    width: 100%;
+    height: 90vh;
+    background-color: darkgreen;
+}
 
 </style>
